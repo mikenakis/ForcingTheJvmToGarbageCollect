0ff7c40910f2ceb45c8c3bdb76d0b344094e645c
Michael <michael@michael.gr>
2022-06-21 01:50:33 +0200

    made the DLL much smaller (under the assumption that the user has MSVCRTxxx.DLL on their system)

--- ForceGarbageCollection/b.bat
+++ ForceGarbageCollection/b.bat
@@ -4,7 +4,7 @@ if not "%Platform%"=="x64" (
     echo Please run this from within the x64 Native Tools Command Prompt of Visual Studio.
 	exit /b 1
 )
-cl /LD /I "C:\Program Files\Java\jdk-17\include" /I "C:\Program Files\Java\jdk-17\include\win32" ForceGarbageCollection.cpp
+cl /LD /MD /I "C:\Program Files\Java\jdk-17\include" /I "C:\Program Files\Java\jdk-17\include\win32" ForceGarbageCollection.cpp
 move ForceGarbageCollection.dll ..
 del ForceGarbageCollection.obj
 del ForceGarbageCollection.exp
